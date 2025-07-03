0de72ee32681dcbf7c681e7b0c059a594ff6a059
salinsalins <sanin_andrey@mail.ru>
2025-07-03 09:23:19 +0700

    Delayed device start

--- Devices/DelayedDevice.py
+++ Devices/DelayedDevice.py
@@ -7,25 +7,22 @@ from PrototypeDumperDevice import *
 
 
 class DelayedDevice(PrototypeDumperDevice):
-    def __init__(self, device_name=None, folder=None, delay = 0.0, **kwargs):
+    def __init__(self, device_name=None, folder=None, delay=0.0, **kwargs):
         if 'dev' in kwargs:
             if device_name is None:
                 device_name = kwargs['dev']
         if device_name is None:
             device_name = 'binp/nbi/adc0'
         super().__init__(device_name, **kwargs)
-        self.save_list = []
-        self.shot_time = []
-        self.zip_file = []
-        self.log_file = []
-        self.delay = delay
-        self.folder = folder
         if folder is None:
             self.folder = device_name
+        self.save_list = []
+        self.delay = delay
+        self.folder = folder
 
     def save(self, log_file, zip_file, folder=None):
         self.save_list.append((time.time(), zip_file, log_file, folder))
-        return
+        return True
 
     def activate(self):
         if self.save_list:
